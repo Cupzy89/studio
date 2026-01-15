bf23a8f674a19ee98822f3b8c35aa3cc61c29639
yusup hardiansyah <yusuph9@gmail.com>
2026-01-15 06:10:29 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/components/inventory-table.tsx
+++ src/components/inventory-table.tsx
@@ -45,7 +45,7 @@ export function InventoryTable() {
           <TableBody>
             {isLoading ? (
               Array.from({ length: 5 }).map((_, index) => (
-                <TableRow key={`skeleton-${index}`}>
+                <TableRow key={`skeleton-row-${index}`}>
                   <TableCell><Skeleton className="h-4 w-24" /></TableCell>
                   <TableCell><Skeleton className="h-4 w-12 float-right" /></TableCell>
                   <TableCell><Skeleton className="h-4 w-20 float-right" /></TableCell>
