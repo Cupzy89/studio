7652742a540a0fbcfed11c1a1ede7f5f3a0a87f4
yusup hardiansyah <yusuph9@gmail.com>
2026-01-19 06:35:10 +0000

    rubah ikon menjadi gulungan kertas (_for element rect_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,27 +7,10 @@ export function Logo() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M14 3V7.6C14 7.86522 14.1054 8.11957 14.2929 8.30711C14.4804 8.49464 14.7348 8.6 15 8.6H19.4"
-        stroke="hsl(var(--primary))"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M12 21H7.6C7.05435 21 6.53043 20.7893 6.15543 20.4142C5.78043 20.0391 5.57 19.5152 5.57 18.96V5.04C5.57 4.48478 5.78043 3.96086 6.15543 3.58579C6.53043 3.21071 7.05435 3 7.6 3H14L19.4 8.6V12"
-        stroke="hsl(var(--primary))"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M16 21.5V16.5M16 16.5L19 19.5M16 16.5L13 19.5"
-        stroke="hsl(var(--accent))"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <path d="M10 2H14" stroke="hsl(var(--primary))" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M10 22H14" stroke="hsl(var(--primary))" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M19.9998 12C19.9998 16.4183 16.4181 20 11.9998 20C7.58156 20 3.99984 16.4183 3.99984 12C3.99984 7.58172 7.58156 4 11.9998 4C14.0537 4 15.9322 4.79374 17.3789 6.07204" stroke="hsl(var(--primary))" strokeWidth="2" strokeLinecap="round"/>
+      <path d="M17 4H20V10" stroke="hsl(var(--accent))" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   );
 }
